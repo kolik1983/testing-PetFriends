1cc0e1d7b482c5e459cdc5cd712f172a98f48819
Alex ER <100380496+kolik1983@users.noreply.github.com>
2022-04-18 21:48:02 +0300

    Add files via upload

---
+++ cats.py
@@ -0,0 +1,7 @@
+from Cat import Cat
+
+baron = Cat('Барон', 'male', 2)
+sam  = Cat('Сэм', 'male', 2)
+
+print("Имя: ", baron.getName(), 'Пол - ', baron.getGender(), 'Возраст - ', baron.getAge())
+print("Имя: ", sam.getName(), 'Пол - ', sam.getGender(), 'Возраст - ', sam.getAge())
